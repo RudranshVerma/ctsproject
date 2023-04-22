e1d3e46939c760d1be497ad83dbc597ef4205b41
RudranshVerma <83331599+RudranshVerma@users.noreply.github.com>
2023-04-22 20:14:25 +0530

    22-4-2023

--- front-end/src/app/login-page/login-page.component.ts
+++ front-end/src/app/login-page/login-page.component.ts
@@ -2,42 +2,29 @@ import { Component ,OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { User } from '../user';
+import { UserloginService } from '../userlogin.service';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
-  public loginForm!:FormGroup;
-  constructor(private formbuilder:FormBuilder,private http:HttpClient,private router:Router){}
 
-  ngOnInit():void{
-    this.loginForm=this.formbuilder.group({
-      email:[''],
-      password:[''],
-      
-    }
-    )
-  }
+  user:User= new User();
+
+  public loginForm!:FormGroup;
+  constructor(private formbuilder:FormBuilder,private userservice: UserloginService,private router:Router){}
+
+  ngOnInit():void{  }
+
+
   logIn(){
-    this.http.get<any>("https://my-data-4hkn.onrender.com/signup").subscribe(
-      res=>{
-        const user= res.find((a:any)=>{
-          return a.email=== this.loginForm.value.email && a.password=== this.loginForm.value.password
-        });
-        if(user){
-          alert("Login Success");
-          this.loginForm.reset();
-          this.router.navigate(['home']);
-        }
-        else{
-          alert('User not found');
-        }
-      },
-      err=>{
-        alert("something went wrong");
-      }
-    )
+    console.log(this.user);
+    this.userservice.loginUser(this.user).subscribe(data=>{
+      alert("login successfully")
+    },error=>alert("sorry enter correct credentials"));
+
   }
 
 
