7a890b24e2220bbf9dea791709870f12ce10bb26
rudransh verma <rudranshverma2323@gmail.com>
2023-05-09 18:21:42 +0530

    login connected to home

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +12,14 @@ export class LoginComponent {
  invalidmessage="Invalid Message";
  invalid=false;
 
+ constructor(private router:Router){}
 
  handlelogin(){
   console.log(this.username+"  "+this.password)
   if(this.username==="yash"&&this.password==="hello"){
     this.invalid=false;
+    this.router.navigate(['home']);
+
   }
   else 
   this.invalid=true;
